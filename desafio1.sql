8e13296739875fa206fa456d95fbbbeb9013177c
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-07-23 18:24:59 -0300

    feat: adiciona conteudo da tabela following_artist

--- desafio1.sql
+++ desafio1.sql
@@ -30,6 +30,7 @@ CREATE TABLE following_artist (
   follow_id INT PRIMARY KEY AUTO_INCREMENT,
   user_id INT NOT NULL,
   artist_id INT NOT NULL,
+  is_following INT NOT NULL DEFAULT 1,
   FOREIGN KEY (user_id) REFERENCES users (user_id),
   FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
 );
@@ -91,12 +92,42 @@ VALUES (1, 1, 1, '2019-10-20'),
 (10, 3, 10, '2017-01-17'),
 ;
 
-INSERT INTO artists()
-VALUES ()
+INSERT INTO artists(artist_id, artist_name)
+VALUES (1, 'Walter Phoenix'),
+(2, 'Peter Strong'),
+(3, 'Lance Day'),
+(4, 'Freedie Shannon'),
+(5, 'Tyler Isle'),
+(6, 'Fog')
 ;
 
-INSERT INTO following_artist ()
-VALUES ()
+    follow_id INT PRIMARY KEY AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  artist_id INT NOT NULL,
+
+INSERT INTO following_artist (follow_id, user_id, artist_id, is_following)
+VALUES (1, 1, 1, 1),
+(2, 1, 4, 1),
+(3, 1, 3, 1),
+(4, 2, 1, 1),
+(5, 2, 3, 1),
+(6, 3, 2, 1),
+(7, 3, 1, 1),
+(8, 4, 4, 1),
+(9, 5, 5, 1),
+(10, 5, 6, 1),
+(11, 6, 6, 1),
+(12, 6, 3, 1),
+(13, 6, 1, 1),
+(14, 7, 2, 1),
+(15, 7, 5, 1),
+(16, 8, 1, 1),
+(17, 8, 5, 1),
+(18, 9, 6, 1),
+(19, 9, 4, 1),
+(20, 9, 3, 1),
+(21, 10, 2, 1),
+(22, 10, 6, 1),
 ;
 
 INSERT INTO albums ()
