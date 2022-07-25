66a69342061bf0b043657ae8d183058a4bf2a66f
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-07-25 19:45:17 -0300

    fix: adiciona ordem por nomeao requisito 3

--- desafio3.sql
+++ desafio3.sql
@@ -5,4 +5,5 @@ FROM users AS u
 INNER JOIN playback_history AS h ON u.user_id = h.user_id
 INNER JOIN songs AS s ON s.song_id = h.song_id AND u.user_id = h.user_id
 GROUP BY h.user_id
+ORDER BY u.user_name
 ;
