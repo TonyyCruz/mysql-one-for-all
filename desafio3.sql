f74b2284bc38f7ef2c4f77921546b322def79495
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-07-25 18:24:23 -0300

    fix: adiciona calculo para converter segundo em munutos

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,6 @@
 SELECT u.user_name AS usuario,
 count(h.USER_id) AS qtde_musicas_ouvidas,
-SUM(s.duration) AS total_minutos
+ROUND(SUM(s.duration) / 60, 2) AS total_minutos
 FROM users AS u
 INNER JOIN playback_history AS h ON u.user_id = h.user_id
 INNER JOIN songs AS s ON s.song_id = h.song_id AND u.user_id = h.user_id
