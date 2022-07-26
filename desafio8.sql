97ddad07a63407eb01b8513f1131d6bbe5d14f96
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-07-25 23:45:52 -0300

    feat: adiciona requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,7 @@
+SELECT art.artist_name AS artista,
+alb.album_name AS album
+FROM artists AS art
+INNER JOIN albums AS alb ON art.artist_id = alb.artist_id
+WHERE art.artist_name LIKE ('WALTER PHOENIX')
+ORDER BY album
+;
