fc803eaff31968ad72443fda90e3ea7244b3b1c8
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-07-31 23:00:04 -0300

    fix: altera o filtro do requisito 10

--- desafio10.sql
+++ desafio10.sql
@@ -4,7 +4,7 @@ FROM songs AS song
 INNER JOIN playback_history AS hist ON song.song_id = hist.song_id
 INNER JOIN users AS u ON hist.user_id = u.user_id
 INNER JOIN plans AS plan ON plan.plan_id = u.plan_id
-AND plan.plan_type = 'pessoal' OR 'gratuito'
+WHERE plan.plan_type IN ('pessoal', 'gratuito')
 
 GROUP BY hist.song_id
 ORDER BY nome
